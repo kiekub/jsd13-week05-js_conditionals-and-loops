6cc4f918967ed713581d319f296877b5270f1814
kie <jutatipr97@gmail.com>
2026-08-01 19:17:14 +0700

    while test:passed

--- exercises/js-conditionals-loops/05-while-loop.js
+++ exercises/js-conditionals-loops/05-while-loop.js
@@ -11,7 +11,18 @@
 // count how many times you can do that before reaching 0.
 
 function countDigits(n) {
-  // TODO: replace this with your implementation
+  n = Math.abs(n);
+
+  if (n === 0) {
+    return 1;
+  }
+
+  let count = 0; 
+  while (n > 0) {
+    count++;
+    n = Math.floor(n/10)
+  }
+  return count;
 }
 
 // --- Tests (do not edit) ---
